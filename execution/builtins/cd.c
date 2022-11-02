0c16eb8279d02f0650bbb98d8dfbf988cc3608f4
zmoussam <zakariamoussammi87@gmail.com>
2022-11-02 17:04:04 +0100

    :)

--- execution/builtins/cd.c
+++ execution/builtins/cd.c
@@ -19,10 +19,7 @@ void	go_to_newpath(char **oldpwd, char *path)
 
 	tmp_cwd = getcwd(NULL, 0);
 	if (chdir(path) == -1)
-	{
-		printf("minishell: cd: %s:%s\n", path, strerror(errno));
-		free(tmp_cwd);
-	}
+		return(printf("minishell: cd: %s:%s\n", path, strerror(errno)), free(tmp_cwd));
 	else
 	{
 		if (oldpwd == NULL)
@@ -35,7 +32,7 @@ void	go_to_newpath(char **oldpwd, char *path)
 	}
 
 }
-void	go_to_oldpath(char **oldpwd, char	*home)
+void	go_to_oldpath(char **oldpwd)
 {
 	char	*tmp_cwd;
 
@@ -44,8 +41,6 @@ void	go_to_oldpath(char **oldpwd, char	*home)
 		return (printf("minishell: cd: OLDPWD not set\n"), free(tmp_cwd));
 	else
 	{
-		if (home == NULL && ft_strcmp(*oldpwd, "/Users/zmoussam") == 0)
-			return (printf("minishell: cd: HOME not set\n"), free(tmp_cwd));
 		if (chdir(*oldpwd) == -1)
 			return (printf("minishell: cd: %s:%s\n", tmp_cwd, strerror(errno)), free(tmp_cwd));
 		else
@@ -56,18 +51,21 @@ void	go_to_oldpath(char **oldpwd, char	*home)
 		}
 	}
 } 
-void	go_to_home(char **oldpwd, char *home)
+void	go_to_home(char **oldpwd)
 {
 	char		*tmp_old;
+	char		*home;
+	t_env_list	*tmp_env_home;
 
+	home = NULL;
+	tmp_env_home = env_find(g_env_list, "HOME", 4);
+	if (tmp_env_home)
+		home = tmp_env_home->content;
 	tmp_old = getcwd(NULL, 0);
 	if (home)
 	{
 		if (chdir(home) == -1)
-		{
-			printf("minishell: cd: %s:%s\n", home, strerror(errno));
-			free(tmp_old);
-		}
+			return(printf("minishell: cd: %s:%s\n", home, strerror(errno)), free(tmp_old));
 		else if (oldpwd == NULL)
 			add_back(&g_env_list, new_node(tmp_old, ft_strdup("OLDPWD"), 6));
 		else
@@ -82,25 +80,19 @@ void	go_to_home(char **oldpwd, char *home)
 void	cd(t_node *root)
 {
 	char		**oldpwd;
-	char		*home;
 	t_env_list *tmp_env_oldpwd;
-	t_env_list *tmp_env_home;
 
 	oldpwd = NULL;
-	home = NULL;
 	tmp_env_oldpwd = env_find(g_env_list, "OLDPWD", 6);
-	tmp_env_home = env_find(g_env_list, "HOME", 4);
 	if (tmp_env_oldpwd)
 		oldpwd = &(tmp_env_oldpwd->content);
-	if (tmp_env_home)
-		home = tmp_env_home->content;
 	if (root->argc == 1 || ft_strcmp(root->argv[1], "~" ) == 0 \
 	|| ft_strcmp(root->argv[1], "--" ) == 0)
-		go_to_home(oldpwd, home);
+		go_to_home(oldpwd);
 	else if (root->argc > 1)
 	{
 		if (ft_strcmp(root->argv[1], "-") == 0)
-			go_to_oldpath(oldpwd, home);
+			go_to_oldpath(oldpwd);
 		else
 			go_to_newpath(oldpwd, root->argv[1]);
 	}
